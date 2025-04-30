c00df6df6f746d3abf06599b6a8421bbc69db654
Alexander <oBelyash@yandex.ru>
2025-04-30 16:37:32 +0300

    Initial Commit

---
+++ Golden_Legends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Golden_Legends
+//
+//  Created by Александр Андреев on 30.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Golden_Legends/Golden_LegendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golden_LegendsApp.swift
+//  Golden_Legends
+//
+//  Created by Александр Андреев on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Golden_LegendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
